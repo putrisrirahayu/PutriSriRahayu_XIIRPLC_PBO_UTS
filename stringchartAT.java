469d8ca2aafb3e90c911a070e13586d9ffe4bff9
putrisrirahayu <59639816+putrisrirahayu@users.noreply.github.com>
2020-09-08 13:39:24 +0700

    Add files via upload

---
+++ stringchartAT.java
@@ -0,0 +1,48 @@
+
+package stringoop;
+
+public class stringchartAT {
+    
+ public static void main (String []args){
+   
+        String a = "ABCDEFGHIJKLMNOPQRSTUVWXYZ#-" ;
+        char hasil = a.charAt(0);
+        
+        char b = a.charAt(15);
+        char c = a.charAt(20);
+        char d = a.charAt(19);
+        char e = a.charAt(17);
+        char f = a.charAt(8);
+        char g = a.charAt(27);
+        char h = a.charAt(18);
+        char i = a.charAt(17);
+         char j = a.charAt(8);
+        char k = a.charAt(27);
+        char l = a.charAt(17);
+        char m = a.charAt(0);
+        char n = a.charAt(7);
+        char o = a.charAt(0);
+        char p = a.charAt(24);
+        char q = a.charAt(20);
+ 
+         System.out.print(b);
+         System.out.print(c);
+         System.out.print(d);
+         System.out.print(e);
+         System.out.print(f);
+         System.out.print(g);
+         System.out.print(h);
+         System.out.print(i);
+         System.out.print(j);
+         System.out.print(k);
+         System.out.print(l);
+         System.out.print(m);
+         System.out.print(n);
+         System.out.print(o);
+         System.out.print(p);
+         System.out.println(q);
+             
+     
+
+}
+}
